129b796da308d3096eef467d8b6254c3f7fea3a8
Gulhan Gurmen <gulgur2202@gmail.com>
2023-09-18 15:43:16 +0300

    Write python code about measure converter (temperature)

---
+++ Measure_Converter_Temperature.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+# This program converts Celsius temperature to Fahrenheit temperature.
+
+celsius = float(input("Please enter the temperature as Celsius  =  "))
+fahrenheit = 32.0 + (1.8 * celsius)
+print("Temperature (F) \t\t\t = ", fahrenheit)
+
+
+# In[ ]:
+
+
+
+
