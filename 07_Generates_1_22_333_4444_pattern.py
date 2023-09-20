42bc759fa8264f9227e708b08008db5d966e8b6d
Gulhan Gurmen <gulgur2202@gmail.com>
2023-09-20 16:49:03 +0300

    Write program that generates 1_22_333_4444 pattern

---
+++ 07_Generates_1_22_333_4444_pattern.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+# This program generates 1-22-333-4444 pattern up to n lines given by user.
+
+n = int(input("Please enter the number of rows : "))
+ 
+for i in range(1, n+1):
+    print(str(i) * i)
+
+
+# In[ ]:
+
+
+
+
