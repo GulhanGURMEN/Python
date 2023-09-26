49859dc37a44bf038e2b76fb547d9224d520cc85
Gulhan Gurmen <gulgur2202@gmail.com>
2023-09-26 16:07:17 +0300

    Write program that prints the multiplication table (1-10)

---
+++ 17_Multiplication_table_(1-10).py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### <div class="alert alert-danger"><font color=black><u>Task :</u> Write program that prints the <font color=red>multiplication table</font> from 1x1=1 to 10x10=100. <b></font></div> 
+
+# In[10]:
+
+
+# This program prints the multiplication table from 1x1=1 to 10x10=100.
+
+for i in range(1,11):
+    print("~" * 20)
+    print(i,"'ler")
+    
+    print("~" * 20)
+    for k in range(1,11):
+        print(i,"x", k,"=",i*k)
+
+
+# In[ ]:
+
+
+
+
