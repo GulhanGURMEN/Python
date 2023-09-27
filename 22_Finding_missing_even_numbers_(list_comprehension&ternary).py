f906496dda91303d05320a317a807ee7979fde71
Gulhan Gurmen <gulgur2202@gmail.com>
2023-09-27 17:20:52 +0300

    Make correction in variable name

--- 22_Finding_missing_even_numbers_(list_comprehension&ternary).py
+++ 22_Finding_missing_even_numbers_(list_comprehension&ternary).py
@@ -14,7 +14,7 @@
 
 jumbled_list = [48, 10, 11, 21, 36, 5, 6, 52, 28, 29, 53, 54, 45, 19, 20, 47, 55, 39,                 41, 7, 9, 17, 26, 27, 42, 22, 37, 51, 46, 18, 44, 30, 34, 13, 15, 35, 33,                 16, 50, 24]
 
-missing_numbers = [i for i in range(min(jumbled_list), max(jumbled_list)+1) if i % 2 == 0 and not i in liste]
+missing_numbers = [i for i in range(min(jumbled_list), max(jumbled_list)+1) if i % 2 == 0 and not i in jumbled_list]
 print(* missing_numbers)
 
 
