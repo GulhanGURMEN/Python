066d18e8d26d9d04eb725eb94528cafe812a9492
Gulhan Gurmen <gulgur2202@gmail.com>
2023-09-27 14:12:02 +0300

    Write a function (user defined) that returns absolute value

---
+++ 20_Absolute_value_(user_defined_function).py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This function returns the absolute value of the given number.
+
+def absolute_value(number) :
+    
+    """This function returns the 
+    absolute value of the given number"""
+    
+    if number > 0 :
+        return number
+    
+    else :
+        return -number
+
+
+# In[6]:
+
+
+absolute_value(-44)
+
+
+# In[8]:
+
+
+# The Python interpreter has a number of functions and types built into it 
+# that are always available. The abs() built-in Python function returns the 
+# absolute value of a number.
+
+abs(-44)
+
